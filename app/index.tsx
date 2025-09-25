3ef26b9bbc4d5746ec959471c996782bead4c157
Kasif Mansuri <mahammadkasifm@zignuts.com>
2025-09-25 14:49:31 +0530

    update: basic setup for development

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,16 @@
 import { Text, View } from "react-native";
+import "./global.css";
 
-export default function Index() {
+export default function App() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-      <Text>Cool</Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-blue-500 font-rubik text-3xl">
+        Welcome to Nativewind!
+      </Text>
+
+      <Text className="text-3xl font-rubik">Kashif</Text>
+      <Text className="text-3xl font-rubik">Welcome to ReState</Text>
+      <Text className="text-3xl">Kashif</Text>
     </View>
   );
 }
