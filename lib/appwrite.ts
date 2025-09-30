7ce11f9bcdccc1c95a8cbae17669a268ed95e6d4
Kasif Mansuri <mahammadkasifm@zignuts.com>
2025-09-30 19:13:28 +0530

    update: add db queries

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -7,6 +7,7 @@ import {
   Client,
   Databases,
   OAuthProvider,
+  Query,
 } from "react-native-appwrite";
 
 export const config = {
@@ -108,6 +109,75 @@ export async function getSessionUser() {
     }
 
     console.log("Get user error: ", error);
+    return null;
+  }
+}
+
+export async function getLatestProperties() {
+  try {
+    const result = await databases.listDocuments(
+      config.databaseId!,
+      config.proppertyTableId!,
+      [Query.orderAsc("$createdAt"), Query.limit(5)],
+    );
+
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+export async function getProperties({
+  filter,
+  query,
+  limit,
+}: {
+  filter: string;
+  query: string;
+  limit?: number;
+}) {
+  try {
+    const buildQuery = [Query.orderDesc("$createdAt")];
+
+    if (filter && filter !== "All")
+      buildQuery.push(Query.equal("type", filter));
+
+    if (query)
+      buildQuery.push(
+        Query.or([
+          Query.search("name", query),
+          Query.search("address", query),
+          Query.search("type", query),
+        ]),
+      );
+
+    if (limit) buildQuery.push(Query.limit(limit));
+
+    const result = await databases.listDocuments(
+      config.databaseId!,
+      config.proppertyTableId!,
+      buildQuery,
+    );
+
+    return result.documents;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+}
+
+// write function to get property by id
+export async function getPropertyById({ id }: { id: string }) {
+  try {
+    const result = await databases.getDocument(
+      config.databaseId!,
+      config.proppertyTableId!,
+      id,
+    );
+    return result;
+  } catch (error) {
+    console.error(error);
     return null;
   }
 }
